1f1e5fe1915b30040b8b792e2c9b71bec30b8c54
connprosser <conn@guerrilla.com.au>
2019-02-26 17:03:36 +1000

    Home

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,9 @@ import Items from '../components/Items';
 
 const Index = (props) => (
   <main className="wide row">
-
+  <section class="intro-home">
+    <h1>Charmaine Yabsley <span>Freelance health journalist</span></h1>
+  </section>
   <Items {...props}></Items>
   </main>
 )
