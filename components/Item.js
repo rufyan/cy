1d5775ebda5b5726f4ff34d89188c189aa0ac223
connprosser <conn@guerrilla.com.au>
2019-02-28 14:03:24 +1000

    test

--- components/Item.js
+++ components/Item.js
@@ -7,8 +7,6 @@ const Item = ((props) => {
   //live images
   //const img = props.gsx$image.$t.startsWith('http') ? props.gsx$image.$t : conf.path + props.gsx$image.$t;
   const img='';
-  //const img = `/static/${props.gsx$image.$t}`;
- //const img = require(`/static/${props.gsx$image.$t}`);
 return (
   <article key={props.id}>
     {
