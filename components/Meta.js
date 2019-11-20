934dca816f03fb55ccc39e6c7f6e449dc70d1093
connprosser <conn@guerrilla.com.au>
2019-11-21 09:36:20 +1000

    FontAwesome local and trimmed

--- components/Meta.js
+++ components/Meta.js
@@ -5,7 +5,10 @@ const Meta = () => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
-    <title>Cyte</title>
+    <meta name="theme-color" content="#416967" />
+    <title>Charmaine Yabsley | Freelance Health journalist</title>
+    <link rel="manifest" href="/static/manifest.json" />
+    <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico" />
   </Head>
 );
 
